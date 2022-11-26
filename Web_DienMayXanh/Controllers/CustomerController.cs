11a9cb690d17063a1c12908e19d7c97a9135e661
MinhHuy <minhhuyduong7@gmail.com>
2022-11-27 00:45:22 +0700

    update-code

---
+++ Web_DienMayXanh/Controllers/CustomerController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web_DienMayXanh.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult CustomerHome()
+        {
+            return View();
+        }
+        public ActionResult Product()
+        {
+            return View();
+        }
+        public ActionResult ModalProduct()
+        {
+            return View();
+        }
+        public ActionResult CartProduct()
+        {
+            return View();
+        }
+
+    }
+}
